6a5869a8c296bd67f3b52e2ac4c2da9c4e91c5bf
R41PC <rakaadrian69@gmail.com>
2022-07-28 10:54:18 +0700

    gajelas

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main(){
     cout << "menambah sesuatu" << endl;
-    cout << "sesuatu dapat terjadi dalam kehidupan ini";
+    cout << "sesuatu dapat terjadi dalam kehidupan ini" << endl;
+    cout << "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";
 }
