538fcc546e8d25733f0529a5bb3aafccb4cac58a
R41PC <rakaadrian69@gmail.com>
2022-07-28 11:00:52 +0700

    entah apa

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
     cout << "menambah sesuatu" << endl;
     cout << "sesuatu dapat terjadi dalam kehidupan ini" << endl;
-    cout << "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";
+    cout << "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";\
+    cout << "entah apa";
 }
